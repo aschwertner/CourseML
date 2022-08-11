cd7d69831dbb9ef4e2b48c06fea6c2326d731e03
Anderson Schwertner <andersonschwertner@hotmail.com>
2022-08-11 18:28:42 -0300

    Adds script to extract data

---
+++ code/time_series_clustering_data_extraction.R
@@ -0,0 +1,14 @@
+rm(list = ls())
+
+# Load dataset and select data of interest
+Drca = read.csv("./dataraw/rca_data_2012_2022-06-29.csv") %>% 
+  select(Date, CellID, DOAVEG_avg) %>% 
+  mutate(Date = as.Date(Date)) %>% 
+  mutate(Month = as.integer(format(Date, "%m")),
+         Year = as.integer(format(Date, "%Y"))) %>% 
+  filter(Year == 2012) %>% 
+  group_by(CellID, Month) %>% 
+  summarise(DOAVEG_avg = mean(DOAVEG_avg))
+
+# Save data in RDS file
+saveRDS(Drca, "./dataderived/Drca.RDS")
